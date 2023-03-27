b8f5a94bfda368abc2d6f62c259774838e6467ca
Varahiil <Uriilpro33@gmail.com>
2023-03-27 23:03:20 +0300

    Task 3 done.

--- src/by/itstep/javatraining/revision/task/Task03.java
+++ src/by/itstep/javatraining/revision/task/Task03.java
@@ -26,6 +26,6 @@ package by.itstep.javatraining.revision.task;
 
 public class Task03 {
     public static int task03(int number) {
-        return 0;
+        return number < 0 ? (number * -1) / 10 % 10 : number /10 % 10;
     }
 }
